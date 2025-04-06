55b9f60a2dbf9f0d21f975d2591b542db861048e
prhebbar <prhebbar@deloitt.com>
2025-04-06 22:49:43 +0530

    updating from ruff_lsp to ruff

--- init.lua
+++ init.lua
@@ -708,7 +708,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        ruff_lsp = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
