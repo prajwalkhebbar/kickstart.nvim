89c74219dbcbf29b42cfa367ff67dc7cf6229b3e
prajwal <prhebbar@deloitte.com>
2025-10-18 23:38:24 +0530

    enabled formatting support for python in init.lua

--- init.lua
+++ init.lua
@@ -786,7 +786,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, python = true, javascript = true, typescript = true, html = true }
+        local disable_filetypes = { c = true, cpp = true, python = false, javascript = true, typescript = true, html = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -801,7 +801,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { 'isort', 'black' },
+        python = { 'ruff', 'isort', 'black', stop_after_first = true },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
